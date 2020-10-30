91b30bfdb629795d045da79a29d171b62d9b7284
Dave Hylands <davehylands@fullstory.com>
2020-10-30 15:16:59 -0700

    Generated code
    Generated using:
    
    cargo generate \
        --git https://github.com/knurling-rs/app-template \
        --branch main \
        --name my-app

---
+++ src/bin/hello.rs
@@ -0,0 +1,11 @@
+#![no_main]
+#![no_std]
+
+use co2_sensor as _; // global logger + panicking-behavior + memory layout
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    defmt::info!("Hello, world!");
+
+    co2_sensor::exit()
+}
---
+++ src/bin/levels.rs
@@ -0,0 +1,15 @@
+#![no_main]
+#![no_std]
+
+use co2_sensor as _; // global logger + panicking-behavior + memory layout
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    defmt::info!("info");
+    defmt::trace!("trace");
+    defmt::warn!("warn");
+    defmt::debug!("debug");
+    defmt::error!("error");
+
+    co2_sensor::exit()
+}
